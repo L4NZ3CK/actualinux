4dae877bcdc1c6ce6891ea4be8c0c7ed4818d8f2
L4NZ3CK <111375475+L4NZ3CK@users.noreply.github.com>
2022-08-17 07:28:06 -1200

    Add files via upload

---
+++ actualinux/actualinux.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+echo "Create by: L4NZ3CK"
+echo "Github: https://github.com/L4NZ3CK"
+echo "Telegram: L4NZ3CK"
+
+sudo apt update
+sudo apt upgrade
+sudo apt install python
+sudo apt install git
+clear
+sudo apt-cache policy scrcpy
+sudo apt install scrcpy
+
